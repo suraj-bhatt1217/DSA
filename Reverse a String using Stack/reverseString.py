f2ee886f695da6cea887085cbba15951fae32489
Suraj Bhatt <surajbhatt54321@gmail.com>
2025-01-03 12:31:33 +0530

    reverse a string using stack

---
+++ Reverse a String using Stack/reverseString.py
@@ -0,0 +1,14 @@
+def reverseString(s):
+    revStack = []
+    # stack = []
+    # for c in s:
+    #     stack.append(c)
+    stack = list(s)
+    for _ in range(len(stack)):
+        revStack.append(stack.pop())
+        
+    return "".join(revStack)
+    
+    
+
+print(reverseString("TechQuiz"))
